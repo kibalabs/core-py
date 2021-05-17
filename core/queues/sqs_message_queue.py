0718035517c9acc60493317c26b306bf21b294ef
Krishan Patel <krishan711@gmail.com>
2021-05-17 23:35:28 +0100

    .

--- core/queues/sqs_message_queue.py
+++ core/queues/sqs_message_queue.py
@@ -15,11 +15,11 @@ class SqsMessageQueue:
     async def send_message(self, message: Message, delaySeconds: int = 0) -> None:
         self.sqsClient.send_message(QueueUrl=self.queueUrl, DelaySeconds=delaySeconds, MessageAttributes={}, MessageBody=message.json())
 
-    async def get_message(self, expectedProcessingSeconds: Optional[int] = None, longPollSeconds: Optional[int] = 0) -> Optional[SqsMessage]:
+    async def get_message(self, expectedProcessingSeconds: int = 300, longPollSeconds: int = 0) -> Optional[SqsMessage]:
         messages = await self.get_messages(limit=1, expectedProcessingSeconds=expectedProcessingSeconds, longPollSeconds=longPollSeconds)
         return messages[0] if messages else None
 
-    async def get_messages(self, limit: Optional[int] = 1, expectedProcessingSeconds: Optional[int] = None, longPollSeconds: Optional[int] = 0) -> Optional[Sequence[SqsMessage]]:
+    async def get_messages(self, limit: int = 1, expectedProcessingSeconds: int = 300, longPollSeconds: int = 0) -> Optional[Sequence[SqsMessage]]:
         sqsResponse = self.sqsClient.receive_message(QueueUrl=self.queueUrl, VisibilityTimeout=expectedProcessingSeconds, MaxNumberOfMessages=limit, WaitTimeSeconds=longPollSeconds)
         sqsMessages = [SqsMessage.from_sqs_message(sqsMessage=sqsMessage) for sqsMessage in sqsResponse.get('Messages', [])]
         return sqsMessages
