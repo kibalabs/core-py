1c0d8aa47250f4bd90d3e03ea4e9f0fb50d485de
Krishan Patel <krishan711@gmail.com>
2021-05-17 22:34:10 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Core-Python
+# Core-Py
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+setupDirectory = os.path.dirname(os.path.realpath(__file__))
+
+requirements = []
+with open(os.path.join(setupDirectory, 'requirements.txt'), 'r') as requirementsFile:
+    for requirement in requirementsFile.read().splitlines():
+        if requirement:
+            requirements.append(requirement)
+
+devRequirements = []
+with open(os.path.join(setupDirectory, 'requirements-dev.txt'), 'r') as requirementsFile:
+    for requirement in requirementsFile.read().splitlines():
+        if requirement:
+            devRequirements.append(requirement)
+
+setup(
+    name='kiba-code',
+    version='0.1.0',
+    description='Kiba Labs\' python utilities',
+    url='https://github.com/kibalabs/core-py',
+    packages=find_packages(exclude=['tests*']),
+    python_requires='~=3.7',
+    install_requires=requirements,
+    tests_require=[],
+    package_data={
+        'core': [
+            'py.typed',
+        ]
+    },
+    test_suite='tests',
+    include_package_data=True,
+    extras_require={
+    },
+)
