726c7ceef0d591ecc1952f9f63011e5f9ea0727d
Krishan Patel <krishan711@gmail.com>
2021-11-19 17:58:45 +0000

    Feature: add redirect exceptions (#16)
    * .
    
    * .
    
    * .

--- core/util/chain_util.py
+++ core/util/chain_util.py
@@ -1,7 +1,8 @@
 import eth_utils
 from web3 import Web3
 
+BURN_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 def normalize_address(value: str) -> str:
     addressBytes = eth_utils.to_bytes(hexstr=value)
-    return Web3.toChecksumAddress(addressBytes[-20:])
+    return str(Web3.toChecksumAddress(addressBytes[-20:]))
