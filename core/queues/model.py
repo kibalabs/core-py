1eb70ff3e96ebd0e6cd5c81ef6e4b9c2830c89bd
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2022-01-17 14:03:43 +0100

    .

--- core/queues/model.py
+++ core/queues/model.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
+import datetime
 
-from typing import Any
+from typing import Any, Optional
 from typing import Dict
 
 from pydantic import BaseModel
+from util import date_util
 
 
 class Message(BaseModel):
     command: str
     content: Dict[str, Any]
+    postDate: Optional[datetime.datetime]
 
 class SqsMessage(Message):
     receiptHandle: str
@@ -30,7 +33,9 @@ class MessageContent(BaseModel):
         return cls._COMMAND
 
     def to_message(self) -> Message:
+        postDate =date_util.datetime_from_now()
         return Message(
             command=self.get_command(),
             content=self.dict(),
+            postDate=postDate,
         )
