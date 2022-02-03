657a689d0be7d361656c3aa174cdd5f7cd698b83
Krishan Patel <krishan711@gmail.com>
2022-02-03 21:01:15 +0000

    KIBA-1339: Add send_messages to SqsMessageQueue (#26)
    * .
    
    * .

--- core/queues/sqs_message_queue.py
+++ core/queues/sqs_message_queue.py
@@ -2,8 +2,10 @@ from typing import TYPE_CHECKING
 from typing import Optional
 from typing import Sequence
 
+from core.exceptions import InternalServerErrorException
 from core.queues.model import Message
 from core.queues.model import SqsMessage
+from core.util import list_util
 
 if TYPE_CHECKING:
     from mypy_boto3_sqs.client import SQSClient
@@ -19,6 +21,22 @@ class SqsMessageQueue:
     async def send_message(self, message: Message, delaySeconds: int = 0) -> None:
         message.set_post_date()
         self.sqsClient.send_message(QueueUrl=self.queueUrl, DelaySeconds=delaySeconds, MessageAttributes={}, MessageBody=message.json())
+
+    async def send_messages(self, messages: Sequence[Message], delaySeconds: int = 0) -> None:
+        failures = []
+        for messageChunk in list_util.generate_chunks(lst=messages, chunkSize=10):
+            requests = []
+            for index, message in enumerate(messageChunk):
+                message.set_post_date()
+                requests.append({'Id': str(index), 'DelaySeconds': delaySeconds, 'MessageAttributes': {}, 'MessageBody': message.json()})
+            response = self.sqsClient.send_message_batch(QueueUrl=self.queueUrl, Entries=requests)
+            failures += response.get('Failed', [])
+        if len(failures) > 0:
+            message = ''
+            for failure in failures:
+                failureType = 'Sender' if failure['SenderFault'] else 'Receiver'
+                message += f'{failureType} fault: id {failure["Id"]}, code {failure["Code"]}, message {failure.get("Message")}\n'
+            raise InternalServerErrorException(message=message)
 
     async def get_message(self, expectedProcessingSeconds: int = 300, longPollSeconds: int = 0) -> Optional[SqsMessage]:
         messages = await self.get_messages(limit=1, expectedProcessingSeconds=expectedProcessingSeconds, longPollSeconds=longPollSeconds)
