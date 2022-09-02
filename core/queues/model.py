12f1d23cb2835498551ff13418f86bdec7126802
Krishan Patel <krishan711@gmail.com>
2022-09-02 14:05:10 +0100

    Feature: Update MessageNeedsReprocessingException to support rescheduling a message (#50)
    * .
    
    * .

--- core/queues/model.py
+++ core/queues/model.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import datetime
+import uuid
 from typing import Any
 from typing import Dict
 from typing import Optional
@@ -13,10 +14,15 @@ from core.util import date_util
 class Message(BaseModel):
     command: str
     content: Dict[str, Any]
+    requestId: Optional[str]
+    postCount: Optional[int]
     postDate: Optional[datetime.datetime]
 
-    def set_post_date(self):
+    def prepare_for_send(self):
+        self.requestId = self.requestId or str(uuid.uuid4()).replace('-', '')
+        self.postCount = (self.postCount + 1) if self.postCount else 1
         self.postDate = date_util.datetime_from_now()
+
 
 class SqsMessage(Message):
     receiptHandle: str
@@ -27,9 +33,12 @@ class SqsMessage(Message):
         return cls(
             command=message.command,
             content=message.content,
+            requestId=message.requestId,
+            postCount=message.postCount,
             postDate=message.postDate,
             receiptHandle=sqsMessage['ReceiptHandle'],
         )
+
 
 class MessageContent(BaseModel):
     _COMMAND = 'UNKNOWN_COMMAND'
@@ -42,5 +51,7 @@ class MessageContent(BaseModel):
         return Message(
             command=self.get_command(),
             content=self.dict(),
-            postDate=None
+            requestId=None,
+            postCount=None,
+            postDate=None,
         )
