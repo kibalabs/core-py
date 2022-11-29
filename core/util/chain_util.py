f1454646f6202fa3dcb48aca148661bec55ab405
Krishan Patel <krishan711@gmail.com>
2022-11-29 10:20:48 +0000

    Fiddle: update dependencies (#60)
    * .
    
    * .

--- core/util/chain_util.py
+++ core/util/chain_util.py
@@ -5,4 +5,4 @@ BURN_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 def normalize_address(value: str) -> str:
     addressBytes = eth_utils.to_bytes(hexstr=value)
-    return str(Web3.toChecksumAddress(addressBytes[-20:]))
+    return str(Web3.to_checksum_address(addressBytes[-20:]))
