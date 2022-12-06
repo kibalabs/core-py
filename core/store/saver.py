16956b945ef4264c374cd77753833da119f82ec6
Krishan Patel <krishan711@gmail.com>
2022-12-06 22:21:52 +0000

    Fix: correct typing for new saver functions (#62)
    * .
    
    * .

--- core/store/saver.py
+++ core/store/saver.py
@@ -5,7 +5,6 @@ from typing import AsyncIterator
 from typing import Dict
 from typing import List
 from typing import Optional
-from typing import TypeVar
 
 from sqlalchemy import Table
 from sqlalchemy.engine import Result
@@ -19,15 +18,14 @@ from core.store.database import ResultType
 if TYPE_CHECKING:
     from sqlalchemy.sql._typing import _ColumnExpressionArgument
     from sqlalchemy.sql._typing import _DMLColumnArgument
+    WhereClause = _ColumnExpressionArgument[bool]
 else:
     _DMLColumnArgument = Any
-    _ColumnExpressionArgument = Any
+    WhereClause = Any
 
 
-ClauseOutput = TypeVar('ClauseOutput')
 CreateRecordValuesDict = Dict[_DMLColumnArgument, Any]  # type: ignore[misc]
 UpdateRecordValuesDict = Dict[_DMLColumnArgument, Any]  # type: ignore[misc]
-WhereClause = _ColumnExpressionArgument[ClauseOutput]
 
 
 class SavingException(InternalServerErrorException):
@@ -60,13 +58,13 @@ class Saver:
         rowId = int(result.scalar_one())
         return rowId
 
-    async def _update_records(self, table: Table, where: WhereClause[bool], values: UpdateRecordValuesDict, connection: Optional[DatabaseConnection] = None) -> List[int]:
+    async def _update_records(self, table: Table, where: WhereClause, values: UpdateRecordValuesDict, connection: Optional[DatabaseConnection] = None) -> List[int]:
         query = table.update().where(where).values(values).returning(table.c.id)
         result = await self._execute(query=query, connection=connection)
         rowIds = [int(rowId) for rowId in result.scalars()]
         return rowIds
 
-    async def _delete_records(self, table: Table, where: WhereClause[bool], connection: Optional[DatabaseConnection] = None) -> List[int]:
+    async def _delete_records(self, table: Table, where: WhereClause, connection: Optional[DatabaseConnection] = None) -> List[int]:
         query = table.delete().where(where).returning(table.c.id)
         result = await self._execute(query=query, connection=connection)
         rowIds = [int(rowId) for rowId in result.scalars()]
