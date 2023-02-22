7620480320eb0854f760d373d46b9bf4324f89e3
Krishan Patel <krishan711@gmail.com>
2023-02-22 16:44:09 +0000

    .

--- setup.py
+++ setup.py
@@ -34,15 +34,15 @@ setup(
     test_suite='tests',
     include_package_data=True,
     extras_require={
-        'api': ['fastapi~=0.82.0', 'uvicorn[standard]~=0.20.0'],
+        'api': ['fastapi~=0.92.0', 'uvicorn[standard]~=0.20.0'],
         'storage': ['aiobotocore~=2.4.2'],
         # TODO(krishan711): remove this in next major update
         'queues': ['aiobotocore~=2.4.1'],
         'queue-sqs': ['aiobotocore~=2.4.1'],
         'queue-aqs': ['azure-storage-queue~=12.5.0'],
-        'database-psql': ['sqlalchemy[asyncio]~=2.0.3', 'asyncpg~=0.27.0'],
+        'database-psql': ['sqlalchemy[asyncio]~=2.0.4', 'asyncpg~=0.27.0'],
         'requester': ['httpx~=0.23.3'],
-        'web3': ['web3==6.0.0-beta.8'],
+        'web3': ['web3==6.0.0-beta.10'],
         'types': [
             'types-aiobotocore[essential]~=2.4.1',
             'types-aiofiles~=22.1.0.4',
