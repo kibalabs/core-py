e8ade15a8523a64e07496f6f152b2d3420b9e02f
Femi <ogunkolaobafemi@gmail.com>
2023-05-10 14:30:50 +0100

    .

--- core/notification_client.py
+++ core/notification_client.py
@@ -1,8 +1,10 @@
 import abc
 from abc import ABC
+from core.requester import KibaResponse
+
 
 class NotificationsClient(ABC):
 
     @abc.abstractmethod
-    async def post(self, messageText: str) -> None:
+    async def post(self, messageText: str) -> KibaResponse:
         pass
