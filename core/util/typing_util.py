b809729b31df51342ac4ba60d608b43793f032b8
Krishan Patel <krishan711@gmail.com>
2023-08-22 12:34:24 +0100

    .

--- core/util/typing_util.py
+++ core/util/typing_util.py
@@ -7,8 +7,8 @@ from typing import TypeAlias
 # NOTE(krishan711): copied from https://github.com/python/typing/issues/182
 JsonBaseType = str | int | float | bool | None
 Json: TypeAlias = Dict[str, "Json"] | Mapping[str, "Json"] | List["Json"] | Sequence["Json"] | JsonBaseType
-JsonDict: TypeAlias = Dict[str, Json] | Mapping[str, Json]
-JsonList: TypeAlias = List[Json] | Sequence[Json]
+JsonDict: TypeAlias = Dict[str, Json]
+JsonList: TypeAlias = List[Json]
 
 # TODO(krishan711): remove this in the next major version
 JSON = Json
