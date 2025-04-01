a43b88e14f40849a6f4fc33dd7b2294ceb65fc29
Krishan Patel <krishan711@gmail.com>
2025-04-01 15:52:49 +0100

    Fiddle: fix for json encoding (#108)

--- core/util/json_util.py
+++ core/util/json_util.py
@@ -1,10 +1,13 @@
+import datetime
 import json
+import typing
 from typing import Any
 
 import orjson
 
 from core import logging
 from core.exceptions import KibaException
+from core.util import date_util
 from core.util.typing_util import Json
 
 
@@ -19,6 +22,16 @@ class JsonEncodeException(KibaException):
 _HAS_LOGGED_FOR_SERIALIZATION_ERROR = False
 
 
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj: typing.Any) -> str:  # type: ignore[explicit-any]
+        if isinstance(obj, datetime.datetime):
+            return date_util.datetime_to_string(obj)
+        try:
+            return typing.cast(str, super().default(obj))
+        except TypeError:
+            return str(obj)
+
+
 def dumpb(obj: Any) -> bytes:  # type: ignore[explicit-any]
     global _HAS_LOGGED_FOR_SERIALIZATION_ERROR  # noqa: PLW0603
     try:
@@ -28,7 +41,7 @@ def dumpb(obj: Any) -> bytes:  # type: ignore[explicit-any]
             if not _HAS_LOGGED_FOR_SERIALIZATION_ERROR:
                 logging.warning(f'There was an error during the serialization an object: `{exception}`, falling back to json.')
                 _HAS_LOGGED_FOR_SERIALIZATION_ERROR = True
-            return json.dumps(obj).encode('utf-8')
+            return json.dumps(obj, cls=DatetimeEncoder).encode('utf-8')
         raise JsonEncodeException(message=str(exception)) from exception
     except orjson.JSONEncodeError as exception:
         raise JsonEncodeException(message=str(exception)) from exception
