0918c064b56e7018e864c4146135270665412640
Krishan Patel <krishan711@gmail.com>
2025-11-01 08:56:28 +0000

    .

--- core/notifications/discord_client.py
+++ core/notifications/discord_client.py
@@ -1,3 +1,4 @@
+from core import logging
 from core.notifications.notification_client import NotificationClient
 from core.requester import KibaResponse
 from core.requester import Requester
@@ -9,6 +10,9 @@ class DiscordClient(NotificationClient):
         self.requester = requester
 
     async def post(self, messageText: str) -> KibaResponse:
+        if len(messageText) > 1000:  # noqa: PLR2004
+            logging.info('Truncating Discord message to 1000 characters')
+            messageText = messageText[:997] + '...'
         data = {'content': messageText}
         response = await self.requester.post(self.webhookUrl, dataDict=data, headers={'content-type': 'application/json'})
         return response
