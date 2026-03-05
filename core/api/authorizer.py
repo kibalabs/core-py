1440a58c180f2afed5461ae23d2b17a926736e70
Krishan Patel <krishan711@gmail.com>
2026-03-05 17:10:01 +0000

    .

--- core/api/authorizer.py
+++ core/api/authorizer.py
@@ -38,7 +38,10 @@ def authorize_bearer_jwt[ApiRequest: BaseModel](  # type: ignore[explicit-any]
         @functools.wraps(func)
         async def async_wrapper(request: KibaApiRequest[ApiRequest]) -> typing.Any:  # type: ignore[explicit-any, misc]
             request.authJwt = await _authorize_bearer_jwt(request=request, authorizer=authorizer)
-            return await func(request=request)
+            result = func(request=request)
+            if hasattr(result, '__aiter__'):
+                return result
+            return await result
 
         # TODO(krishan711): figure out correct typing here
         return async_wrapper  # type: ignore[return-value]
