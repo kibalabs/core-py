276d2f84b4cd6f2d55f90f794971a2050cda5ac7
Krishan Patel <krishan711@gmail.com>
2026-09-16 14:38:15 +0100

    .

--- core/util/typing_util.py
+++ core/util/typing_util.py
@@ -1,7 +1,5 @@
-from collections.abc import Mapping
-from collections.abc import Sequence
+from pydantic import JsonValue
 
-JsonBaseType = str | int | float | bool | None
-type Json = dict[str, 'Json'] | Mapping[str, 'Json'] | list['Json'] | Sequence['Json'] | JsonBaseType
-type JsonObject = dict[str, 'Json']
-type JsonList = list['Json']
+type Json = JsonValue
+type JsonObject = dict[str, JsonValue]
+type JsonList = list[JsonValue]
