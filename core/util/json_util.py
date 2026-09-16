db87b8f57e46a3b15f010bb56a3c05f01083c312
Krishan Patel <krishan711@gmail.com>
2026-09-16 16:42:35 +0100

    .

--- core/util/json_util.py
+++ core/util/json_util.py
@@ -23,7 +23,7 @@ _HAS_LOGGED_FOR_SERIALIZATION_ERROR = False
 def dumpb(obj: Any) -> bytes:  # type: ignore[explicit-any]
     global _HAS_LOGGED_FOR_SERIALIZATION_ERROR  # noqa: PLW0603
     try:
-        # Keep UTC datetime output aligned with pydantic's ISO 8601 format.
+        # NOTE(krishan711): Keep UTC datetime output aligned with pydantic's ISO 8601 format.
         return orjson.dumps(obj, option=orjson.OPT_UTC_Z)
     except TypeError as exception:
         if str(exception) == 'Integer exceeds 64-bit range':
