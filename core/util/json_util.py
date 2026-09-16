bc89235a378a81a4fbef84f36feb0e3db3e12bbb
Krishan Patel <krishan711@gmail.com>
2026-09-16 16:37:55 +0100

    .

--- core/util/json_util.py
+++ core/util/json_util.py
@@ -2,8 +2,7 @@ import typing
 from typing import Any
 
 import orjson
-from pydantic_core import from_json
-from pydantic_core import to_json
+import pydantic_core
 
 from core import logging
 from core.exceptions import KibaException
@@ -28,11 +27,11 @@ def dumpb(obj: Any) -> bytes:  # type: ignore[explicit-any]
     except TypeError as exception:
         if str(exception) == 'Integer exceeds 64-bit range':
             if not _HAS_LOGGED_FOR_SERIALIZATION_ERROR:
-                logging.warning(f'There was an error during the serialization an object: `{exception}`, falling back to json.')
+                logging.warning(f'There was an error during the serialization an object: `{exception}`, falling back to pydantic.')
                 _HAS_LOGGED_FOR_SERIALIZATION_ERROR = True
             # NOTE(krishan711): orjson is faster, but doesnt support ints bigger than 64bit
             # so we use pydantic instead (still faster than python json)
-            return to_json(obj, fallback=str, inf_nan_mode='null')
+            return pydantic_core.to_json(obj, fallback=str, inf_nan_mode='null')
         raise JsonEncodeException(message=str(exception)) from exception
 
 
@@ -44,6 +43,6 @@ def loads(json: str | bytes | bytearray) -> Json:
     try:
         # NOTE(krishan711): orjson is faster, but doesnt support ints bigger than 64bit
         # so we use pydantic instead (still faster than python json)
-        return typing.cast(Json, from_json(json, allow_inf_nan=False))
+        return typing.cast(Json, pydantic_core.from_json(json, allow_inf_nan=False))
     except ValueError as exception:
         raise JsonDecodeException(message=str(exception)) from exception
